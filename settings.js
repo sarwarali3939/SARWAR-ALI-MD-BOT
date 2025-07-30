31557ce53c39835c030173ee076833bd285eb559
sarwarali3939 <sjjddihwpsjd@gmail.com>
2025-07-30 13:09:31 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "rVpTnAIQ#rgBq3ZcCmpRhr-GBD2HEDDEPlBoVnTY0wgBRWyI5B7Y", // Add your session id
+PREFIX: process.env.PREFIX || ".",
+MODE: process.env.MODE || "public",  //public | private | inbox | group
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923188211549",
+DEV: process.env.DEV || "923188211549",
+LANG: process.env.LANG || "EN", // EN | SI
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "*_`ᴀᴜᴛᴏ ꜱᴛᴀᴛᴜꜱ ᴊᴜꜱᴛ ɴᴏᴡ ꜱᴇᴇɴ ʙʏ ʟᴀʀᴀ ᴍᴅ`_* 💃🏻",
+AUTO_BIO: process.env.AUTO_BIO || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",    
+AUTO_REACT: process.env.AUTO_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_BAD: process.env.ANTI_BAD || "true",   
+ANTI_BOT: process.env.ANTI_BOT || "false",
+ANTI_CALL: process.env.ANTI_CALL || "true",
+BAD_NUMBER_BLOCKER: process.env.BAD_NUMBER_BLOCKER || "false",
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+READ_CMD: process.env.READ_CMD || "true",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ANTI_VV: process.env.ANTI_VV || "true",
+ANTI_DELETE: process.env.ANTI_DELETE || "true",
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same",  // inbox | same
+WELCOME: process.env.WELCOME || "true",
+GOODBYE: process.env.GOODBYE || "true",
+ADMIN_EVENTS: process.env.ADMIN_EVENTS || "true",
+INBOX_BLOCK: process.env.INBOX_BLOCK || "false",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+};
